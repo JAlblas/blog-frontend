280cc30b121ef3f4c81327105bec05bf6ca10aff
Jasper Alblas <just_jasper@hotmail.com>
2021-07-27 22:29:02 +0200

    backend token now saved in react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Posts from './components/Posts';
@@ -9,6 +9,11 @@ import Register from './components/Register';
 import Publish from './components/Publish';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
   return (
     <div className="App">
       <h1>Application</h1>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,48 @@
-import './Login.css';
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Login() {
+import './Login.css';
+
+async function loginUser(credentials) {
+  return fetch('http://localhost:3000/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+    .then(data => data.json())
+ }
+
+export default function Login({ setToken }) {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const token = await loginUser({
+      email,
+      password
+    });
+    console.log(token);
+    setToken(token);
+  }
+
   return(
-    <form action="http://localhost:3000/login" method="post">
-      <label>
-        <p>Email</p>
-        <input type="email" placeholder="Enter Email" name="email" required/>
-      </label>
-      <label>
-        <p>Password</p>
-        <input type="password" placeholder="Enter Password" name="password" require/>
-      </label>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+    <div className="login-wrapper">
+    <h1>Please Log In</h1>
+      <form  method="post" onSubmit={handleSubmit}>
+        <label>
+          <p>Email</p>
+          <input type="email" placeholder="Enter Email"  onChange={e => setEmail(e.target.value)}/>
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" placeholder="Enter Password" onChange={e => setPassword(e.target.value)}/>
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
   )
 }
