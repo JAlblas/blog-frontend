219627760012598928c6e31c974994dcba406783
Jasper Alblas <just_jasper@hotmail.com>
2021-07-27 11:26:19 +0200

    moved fetching code to posts component

--- src/App.js
+++ src/App.js
@@ -3,25 +3,13 @@ import Posts from './Posts';
 import Login from './Login';
 import Register from './Register';
 
-import { useState, useEffect } from 'react';
-
 function App() {
-
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    // Update the document title using the browser API
-    fetch('http://localhost:3000/posts')
-    .then(response => response.json())
-    .then(data => setPosts(data));
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
         <nav>Welcome to my blog!</nav>
       </header>
-      <Posts posts={posts}/>
+      <Posts/>
       <Login/>
       <Register/>
     </div>
--- src/Posts.js
+++ src/Posts.js
@@ -1,12 +1,21 @@
 import './Posts.css';
-
 import Post from './Post';
+import { useState, useEffect } from 'react';
 
 function Posts(props) {
 
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    fetch('http://localhost:3000/posts')
+    .then(response => response.json())
+    .then(data => setPosts(data));
+  }, []);
+
   return (
     <div className="posts">
-        {props.posts.map(function(post, i){
+        {posts.map(function(post, i){
             return <Post post={post}/>;
         })}
     </div>
